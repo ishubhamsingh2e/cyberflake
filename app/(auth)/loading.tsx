fb2841530cba18a97e78a8c7effadb4189655f38
Shubham Singh <ishubhamsingh2e@gmail.com>
2024-02-28 20:25:06 +0530

    new loading screen

--- app/(auth)/loading.tsx
+++ app/(auth)/loading.tsx
@@ -1,50 +1,19 @@
-"use client";
-
 import { Icons } from "@/components/icons";
-import React, { useState, useEffect } from "react";
 
 function Loading() {
-    const [currentIcon, setCurrentIcon] = useState(1);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIcon((prevIcon) => (prevIcon % 3) + 1);
-        }, 1500);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const getIcon = () => {
-        switch (currentIcon) {
-            case 1:
-                return (
-                    <div className="flex flex-col justify-center items-center text-slate-50 text-xl space-y-2">
-                        <Icons.build className="w-20 h-20" />
-                        <label className="text-slate-50">build</label>
-                    </div>
-                );
-            case 2:
-                return (
-                    <div className="flex flex-col justify-center items-center text-slate-50 text-xl space-y-2">
-                        <Icons.create className="w-20 h-20" />
-                        <label className="text-slate-50">create</label>
-                    </div>
-                );
-            case 3:
-                return (
-                    <div className="flex flex-col justify-center items-center text-slate-50 text-xl space-y-2">
-                        <Icons.evolve className="w-20 h-20" />
-                        <label className="text-slate-50">evolve</label>
-                    </div>
-                );
-            default:
-                return null;
-        }
-    };
-
     return (
-        <div className="bg-primary h-screen w-screen flex justify-center items-center">
-            {getIcon()}
+        <div className="bg-primary h-screen w-screen flex justify-center items-center space-x-3">
+            <div className="flex flex-col justify-center text-center text-slate-50 space-y-2 text-xl animate-bounce duration-500">
+                <Icons.build className="w-16 h-20" />
+            </div>
+
+            <div className="flex flex-col justify-center text-center text-slate-50 space-y-2 text-xl animate-bounce delay-100 duration-500">
+                <Icons.create className="w-20 h-20" />
+            </div>
+
+            <div className="flex flex-col justify-center text-center text-slate-50 space-y-2 animate-bounce delay-200 duration-500">
+                <Icons.evolve className="w-16 h-20" />
+            </div>
         </div>
     );
 }
