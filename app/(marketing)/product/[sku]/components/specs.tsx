14bd70090f763dbbd70cfeb195643f6408235bd9
Shubham Singh <ishubhamsingh2e@gmail.com>
2024-03-01 16:04:40 +0530

    added: merge same attribute in table

--- app/(marketing)/product/[sku]/components/specs.tsx
+++ app/(marketing)/product/[sku]/components/specs.tsx
@@ -6,12 +6,29 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import React from "react";
 
 interface SpecsTableProps {
     specs: Array<{ attribute: string; value: string }>;
 }
 
 export function SpecsTable({ specs }: SpecsTableProps) {
+    // Helper function to group specs by attribute
+    const groupSpecsByAttribute = () => {
+        const groupedSpecs: { [key: string]: Array<string> } = {};
+
+        specs.forEach((spec) => {
+            if (!groupedSpecs[spec.attribute]) {
+                groupedSpecs[spec.attribute] = [];
+            }
+            groupedSpecs[spec.attribute].push(spec.value);
+        });
+
+        return groupedSpecs;
+    };
+
+    const groupedSpecs = groupSpecsByAttribute();
+
     return (
         <Table>
             <TableHeader>
@@ -21,11 +38,25 @@ export function SpecsTable({ specs }: SpecsTableProps) {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                {specs.map((spec, index) => (
-                    <TableRow key={index}>
-                        <TableCell>{spec.attribute}</TableCell>
-                        <TableCell><p>{spec.value}</p></TableCell>
-                    </TableRow>
+                {Object.keys(groupedSpecs).map((attribute, index) => (
+                    <React.Fragment key={index}>
+                        <TableRow>
+                            <TableCell
+                                className="font-medium"
+                                rowSpan={groupedSpecs[attribute].length}
+                            >
+                                {attribute}
+                            </TableCell>
+                            <TableCell>{groupedSpecs[attribute][0]}</TableCell>
+                        </TableRow>
+                        {groupedSpecs[attribute]
+                            .slice(1)
+                            .map((value, subIndex) => (
+                                <TableRow key={`${index}-${subIndex}`}>
+                                    <TableCell>{value}</TableCell>
+                                </TableRow>
+                            ))}
+                    </React.Fragment>
                 ))}
             </TableBody>
         </Table>
