01c95405f44131147930636d98c6773019e4640d
Shubham Singh <ishubhamsingh2e@gmail.com>
2024-03-03 14:51:30 +0530

    path fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,11 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { URL } from "./env"
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
-export function getAssest(image: string) {
-    return `${URL}/${image}`
+export function getAssest(assest: string) {
+    return `${URL}${assest}`
 }
